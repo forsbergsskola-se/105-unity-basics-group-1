2283b3d95fa2ee4260fa5bbdb5294c431cfa02cf
QaleQ <aleflard@gmail.com>
2022-10-30 19:27:01 +0100

    create car object

---
+++ IndividualProjects/FirstNameLastName/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 150f;
+    [SerializeField] float movementSpeed = 6f;
+    void FixedUpdate() {
+        float passedTime = Time.deltaTime;
+        
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * passedTime * movementSpeed);
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * passedTime * rotationSpeed, 0f);
+    }
+}
