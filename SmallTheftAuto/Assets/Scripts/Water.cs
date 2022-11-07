f8a992b73c8839297ec248c2d4dd6c45e1c6dfd9
nihanbir <nihan.bir@gmail.com>
2022-11-07 16:46:31 +0100

    Creates water prefab with logic

--- SmallTheftAuto/Assets/Scripts/Water.cs
+++ SmallTheftAuto/Assets/Scripts/Water.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Player player;
+    void OnTriggerEnter(Collider col)
     {
-        
+        if (col.gameObject.CompareTag("Player")) {
+            player = col.GetComponent<Player>();
+            Invoke("DealDamage", 2f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerExit(Collider other) {
+        CancelInvoke("DealDamage");
+    }
+
+    void DealDamage() {
+        player.playerInfo.Health = 0;
     }
 }
