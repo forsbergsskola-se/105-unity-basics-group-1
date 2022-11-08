73fcc37137f8ab28ab8ab874fe2bd1e99543b74d
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-11-08 16:05:05 +0100

    Fix merge conflict

--- SmallTheftAuto/Assets/Scripts/Camera.cs
+++ SmallTheftAuto/Assets/Scripts/Camera.cs
@@ -5,12 +5,7 @@ public class Camera : MonoBehaviour {
     public GameObject target;
     public float cameraSpeed;
     private Vector3 offset = new Vector3(0f, 10f, 0f);
-<<<<<<< Updated upstream
-    private Vector3 velocity;
-    void Update() {
-        transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset, ref velocity, cameraSpeed);
-=======
-    
+
     //testing
     public Transform target2;
     public Vector3 offset2 = new Vector3(0, 4, 0);
@@ -19,8 +14,7 @@ public class Camera : MonoBehaviour {
 
     void Update() {
         //transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * cameraSpeed);
->>>>>>> Stashed changes
-        
+
         // locked on camera
         // transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10, player.transform.position.z);
         
