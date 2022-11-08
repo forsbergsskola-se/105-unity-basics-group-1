24ef6235e6fb23b2e9f0d69d45c455033d784644
nihanbir <nihan.bir@gmail.com>
2022-11-08 10:10:18 +0100

    Adds ChangeTarget method to the Camera script

--- SmallTheftAuto/Assets/Scripts/Camera.cs
+++ SmallTheftAuto/Assets/Scripts/Camera.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
 
-    public GameObject player;
+    public GameObject target;
     public float cameraSpeed;
     private Vector3 offset = new Vector3(0f, 10f, 0f);
     void Update() {
-        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, Time.deltaTime * cameraSpeed);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * cameraSpeed);
         
         // locked on camera
         // transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 10, player.transform.position.z);
+    }
+
+    void ChangeTarget(GameObject target)
+    {
+        this.target = target;
     }
 }
