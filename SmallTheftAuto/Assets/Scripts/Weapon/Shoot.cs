73a2358e6928b242176ffa917b5e30f49df41acf
Stebbi Halldors <stebbi.halldors@student.forsbergsskola.se>
2022-11-11 11:44:15 +0100

    Code cleanup on shoot script

--- SmallTheftAuto/Assets/Scripts/Weapon/Shoot.cs
+++ SmallTheftAuto/Assets/Scripts/Weapon/Shoot.cs
@@ -6,7 +6,7 @@ public class Shoot : MonoBehaviour
     public WeaponInfo weaponInfo;
     private Transform _firePoint;
     private Weapon _weapon;
-    private float shootTimer;
+    private float _shootTimer;
     private bool _canShoot;
     
     private void Start()
@@ -18,42 +18,28 @@ public class Shoot : MonoBehaviour
 
     private void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Mouse0) && !_weapon.isReloading && weaponInfo.nameOfWeapon == "HandGun")
-        {
-            var transform1 = _firePoint.transform;
-            Instantiate(bullet, transform1.position, transform1.rotation );
-            _weapon.currentAmmo--;
-            
-            weaponInfo.currentAmmo = _weapon.currentAmmo;
-        }
-
-        if (Input.GetKey(KeyCode.Mouse0) && !_weapon.isReloading && weaponInfo.nameOfWeapon == "MachineGun")
+            InstantiatingBullets();
+        
+        if(Input.GetKey(KeyCode.Mouse0) && !_weapon.isReloading &&weaponInfo.nameOfWeapon == "MachineGun")
         {
             Timer();
             if(_canShoot)
             {
-                var transform1 = _firePoint.transform;
-                Instantiate(bullet, transform1.position, transform1.rotation );
-                _weapon.currentAmmo--;
-                
-                weaponInfo.currentAmmo = _weapon.currentAmmo;
-                
+                InstantiatingBullets();
                 _canShoot = false;
-                shootTimer = 0.14f;
+                _shootTimer = 0.14f;
             }
         }
-
+        
         if (Input.GetKeyDown(KeyCode.Mouse0) && weaponInfo.nameOfWeapon == "Fists")
         {
-            var ray = new Ray(this.transform.position, this.transform.forward);
+            var ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 1f))
             {
                 if (hit.collider.GetComponent<Vehicle>())
-                {
                     hit.collider.GetComponent<Vehicle>().TakeDamage(10);
-                }
             }
             
         }
@@ -62,11 +48,18 @@ public class Shoot : MonoBehaviour
 
     private void Timer()
     {
-        if (shootTimer > 0)
-            shootTimer -= Time.deltaTime;
-        if(shootTimer < 0)
-        {
+        if (_shootTimer > 0)
+            _shootTimer -= Time.deltaTime;
+        if(_shootTimer < 0)
             _canShoot = true;
-        }
+    }
+
+    private void InstantiatingBullets()
+    {
+        var transform1 = _firePoint.transform;
+        Instantiate(bullet, transform1.position, transform1.rotation );
+        _weapon.currentAmmo--;
+                
+        weaponInfo.currentAmmo = _weapon.currentAmmo;
     }
 }
