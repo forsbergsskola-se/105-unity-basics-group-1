9d07909398d7c9f917f782e7bc3f3c998298112e
QaleQ <aleflard@gmail.com>
2022-11-11 13:46:48 +0100

    finalize path logic

--- SmallTheftAuto/Assets/Scripts/Path.cs
+++ SmallTheftAuto/Assets/Scripts/Path.cs
@@ -1,19 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Path : MonoBehaviour
-{
-    public GameObject prefab;
-    private Transform paths;
-    public GameObject car;
-    void Start()
-    {
-        paths.GetChild(0);
+public class Path : MonoBehaviour {
+    public PathConfig pathConfig;
+    private List<Transform> _wayPoints;
+    public int _currentWaypoint = 0;
+    
+    void Start() {
+        _wayPoints = pathConfig.GetWaypoints();
         
-        car.tag = "Player";
     }
 
-    void Update()
-    {
-        
+    void Update() {
+        FollowPath();
+    }
+
+    private void FollowPath() {
+        Vector3 targetPos = _wayPoints[_currentWaypoint].position;
+        Vector3 targetDir = targetPos - transform.position;
+        float delta = pathConfig.GetMoveSpeed() * Time.deltaTime;   
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDir, delta, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, delta);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+        if (transform.position == targetPos) _currentWaypoint++;
+        if (_currentWaypoint == _wayPoints.Count) _currentWaypoint = 0;
     }
 }
